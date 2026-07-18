dd76c89f6bb4322674c014c7cf863f2c7f4638f7
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-18 14:17:27 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,10 +4,17 @@ resource "google_compute_network" "vpc" {
   routing_mode            = "REGIONAL"
 }
 
+
 resource "google_compute_subnetwork" "subnet" {
   name                     = "${var.vpc_name}-subnet"
   ip_cidr_range            = var.subnet_cidr
   region                   = var.region
   network                  = google_compute_network.vpc.id
   private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
